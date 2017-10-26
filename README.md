513361411c18dad90e2e958793c611fd2f987c10
SEKINE, Hideki <sekineh@toyo.co.jp>
2017-10-26 22:57:49 +0900

    Add Iterator trait for PcapReaderSeek. Doc update.

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# pcap-random-access
+# pcap-file-index
 
-This crate provides random access for pcap files.
+This crate provides O(1) index access for packets in a pcap file.
+You can access to the n'th packet in the pcap file in constant time.
 
-## comparison matrix
+## Usage
+
+Add to Cargo.toml:
+```
+[dependency]
+pcap-file = "0.7.0"
+pcap-file-index = "" 
+```
+I tested with pcap-file 0.7.0 on windows platform.
+
+## Features
+- PcapReader Wrapper that extracts offsets from pcap file
+
+## TODO
+- create index file that help random access of pcap
+- implement Index trait for PcapReader
+
+## Comparison matrix 
+The dependency on pcap-file was determined based on the following criteria.
 
 - pcap 0.7.0 - wrapper of pcap/WinPcap
   - support other than little endian: yes
@@ -12,6 +31,7 @@ This crate provides random access for pcap files.
   - support other than little endian: yes
   - support nanosecond pcap: yes
   - support write: yes
+  - can access to undelying reader: yes
 
 - pcap-rs 1.0.1 - nom-based pcap parser
   - support other than little endian: yes
@@ -22,8 +42,10 @@ This crate provides random access for pcap files.
   - support other than little endian: yes
   - support nanosecond pcap: yes
   - support write: yes
+  - can access to undelying reader: no
 
 - pcapng 1.0.0 - nom-based pcapng-only parser
   - support other than little endian: no
     - https://github.com/richo/pcapng-rs/issues/1
   - support nanosecond pcap: no
+  - support pcap file: no
--- src/lib.rs
+++ src/lib.rs
@@ -2,52 +2,53 @@ extern crate pcap_file;
 
 use std::io;
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use pcap_file::*;
-use pcap_file::errors::*;
+// use pcap_file::errors::ResultChain;
 
 pub struct PcapReaderSeek<R>
-    where R: io::Read + io::Seek
+    where R: Read + Seek
 {
     pub inner: PcapReader<R>,
 }
 
 impl<R> PcapReaderSeek<R>
-    where R: io::Read + io::Seek
+    where R: Read + Seek
 {
     pub fn new(reader: R) -> errors::ResultChain<PcapReaderSeek<R>> {
         Ok(PcapReaderSeek { inner: PcapReader::new(reader)? })
     }
 
     pub fn tell(&mut self) -> io::Result<u64> {
-        self.seek(io::SeekFrom::Current(0))
+        self.seek(SeekFrom::Current(0))
     }
 }
 
-impl<R> io::Seek for PcapReaderSeek<R>
-    where R: io::Read + io::Seek
+impl<R> Seek for PcapReaderSeek<R>
+    where R: Read + Seek
 {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
         let file = self.inner.get_mut();
         file.seek(pos)
     }
 }
 
-// impl<R> Iterator for PcapReaderSeek<R>
-//     where R: io::Read + io::Seek
-// {
-//     type Item = ResultChain<Packet<'static>>;
+impl<R: Read + Seek> Iterator for PcapReaderSeek<R>
+{
+    type Item = Packet<'static>;
 
-//     fn next(&mut self) -> Option<Self::Item> {
-//         self.inner.next()
-//     }
-// }
+    fn next(&mut self) -> Option<Self::Item> {
+        // Iterator::next(&mut self.inner)
+        self.inner.next()
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use std::fs::File;
     use super::*;
     #[test]
-    fn it_works() {
+    fn sanity() {
         let file = File::open("tests/test_in.pcap").expect("Error opening file");
         let mut pcap_reader = PcapReaderSeek::new(file).unwrap();
 
@@ -57,7 +58,7 @@ mod tests {
             let offset = pcap_reader.tell().unwrap();
             assert_eq!(offset, calc_offset);
             // println!("offset: {:?}", offset);
-            if let Some(pkt) = pcap_reader.inner.next() {
+            if let Some(pkt) = pcap_reader.next() {
                 // println!("pkt: {:?}", pkt);
                 calc_offset = offset + 16 + pkt.header.incl_len as u64;
                 // println!("tell + 16 + pkt.incl_len: {}", calc_offset);
