8e28374dc11f46a61df96d0b11f227b8187ef801
SEKINE, Hideki <sekineh@toyo.co.jp>
2017-10-27 19:43:52 +0900

    Add PcapReaderIndex

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,72 @@ use std::io::prelude::*;
 use std::io::SeekFrom;
 use pcap_file::*;
 
+use std::fs::File;
+use std::io::BufReader;
+
+/// PcapReader that support Index trait
+struct PcapReaderIndex {
+    pub inner: PcapReaderSeek<BufReader<File>>,
+    index: FileOffsetIndex,
+}
+
+impl PcapReaderIndex {
+    pub fn new(path: &str) -> errors::ResultChain<PcapReaderIndex> {
+        let index = FileOffsetIndex::from_pcap_path(path)?;
+        let file = File::open(path)?;
+        let buf = std::io::BufReader::new(file);
+        Ok(PcapReaderIndex {
+            inner: PcapReaderSeek::new(buf)?,
+            index: index,
+        })
+    }
+
+    fn index_file_name(path: &str) -> String {
+        format!("{}.index", path)
+    }
+
+    pub fn access(&mut self, index: usize) -> Packet<'static> {
+        let offset = self.index.inner[index];
+        self.inner.seek(SeekFrom::Start(offset));
+        self.inner.next().unwrap()
+    }
+}
+
+// impl std::ops::Index<usize> for PcapReaderIndex {
+//     type Output = Packet<'static>;
+//     fn index(&self, index: usize) -> Self::Output {
+//         let offset = self.index.inner[index];
+//         unsafe {
+//             let reader: &mut PcapReaderSeek<BufReader<File>> = std::mem::transmute(&self.inner);
+//             reader.seek(SeekFrom::Start(offset));
+//             reader.next().unwrap()
+//         }
+//     }
+// }
+
+/// Store packet offsets included within the pcap file
+#[derive(Debug)]
+struct FileOffsetIndex {
+    inner: Vec<u64>,
+}
+
+impl FileOffsetIndex {
+    pub fn from_pcap_path(pcap: &str) -> errors::ResultChain<Self> {
+        let mut reader = PcapReaderSeek::new(std::fs::File::open(pcap)?).unwrap();
+        let mut inner: Vec<u64> = Vec::new();
+
+        loop {
+            inner.push(reader.tell()?);
+            match reader.next() {
+                Some(_) => continue,
+                None => break,
+            }
+        }
+
+        Ok(FileOffsetIndex { inner: inner })
+    }
+}
+
 /// Thin wrapper to the PcapReader that support Seek and Iterator traits.
 ///
 /// The functionality is minimum, use inner field to access more features.
@@ -37,8 +103,7 @@ impl<R> Seek for PcapReaderSeek<R>
     }
 }
 
-impl<R: Read + Seek> Iterator for PcapReaderSeek<R>
-{
+impl<R: Read + Seek> Iterator for PcapReaderSeek<R> {
     type Item = Packet<'static>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -47,10 +112,11 @@ impl<R: Read + Seek> Iterator for PcapReaderSeek<R>
     }
 }
 
-#[cfg(test)]
+// #[cfg(test)]
 mod tests {
     use std::fs::File;
     use super::*;
+
     #[test]
     fn sanity() {
         let file = File::open("tests/test_in.pcap").expect("Error opening file");
@@ -70,5 +136,13 @@ mod tests {
                 break;
             }
         }
+    }
+
+    #[test]
+    fn pcap_access() {
+        let mut reader = PcapReaderIndex::new("tests/test_in.pcap").expect("Error opening pcap file");
+        println!("1: {:?}", reader.access(1));
+        println!("0: {:?}", reader.access(0));
+        println!("5: {:?}", reader.access(5));
     }
 }
